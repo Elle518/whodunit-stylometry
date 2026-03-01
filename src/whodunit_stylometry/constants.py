0fcd94ec11a2809817366939555a3cc03daa5208
Elle518 <teregc@hotmail.es>
2026-03-01 23:02:48 +0100

    Remove double-quote-string-fixer hook

---
+++ src/whodunit_stylometry/constants.py
@@ -0,0 +1,10 @@
+"""Constants used across the project."""
+
+AUTHORS_SHORTNAME_MAP = {
+    "anna_katharine_green": "Green",
+    "arthur_conan_doyle": "Doyle",
+    "arthur_morrison": "Morrison",
+    "gilbert_keith_chesterton": "Chesterton",
+    "richard_austin_freeman": "Freeman",
+    "wilkie_collins": "Collins",
+}
