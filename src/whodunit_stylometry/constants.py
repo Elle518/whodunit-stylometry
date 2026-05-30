59173878ad713ae33670da998b3ac94b8a229318
Elle518 <teregc@hotmail.es>
2026-05-30 20:51:04 +0200

    Remove unused constant

--- src/whodunit_stylometry/constants.py
+++ src/whodunit_stylometry/constants.py
@@ -1,14 +1,5 @@
 """Constants used across the project."""
 
-AUTHORS_SHORTNAME_MAP = {
-    "anna_katharine_green": "Green",
-    "arthur_conan_doyle": "Doyle",
-    "arthur_morrison": "Morrison",
-    "gilbert_keith_chesterton": "Chesterton",
-    "richard_austin_freeman": "Freeman",
-    "wilkie_collins": "Collins",
-}
-
 AUTHORS_ABREV_MAP = {
     "Anna Katharine Green": "A.K. Green",
     "Arthur Conan Doyle": "A.C. Doyle",
